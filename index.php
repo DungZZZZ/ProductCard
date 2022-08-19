456177658b23e98cb370db6ea6f06b1337821fcf
DungZZZZ <89644538+DungZZZZ@users.noreply.github.com>
2022-08-19 11:18:23 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php header( 'Location: /Nike Zoom KD 12 Green.html' ) ;  ?>
+<?php header( 'Location: /Nike Zoom KD 12 Orange.html' ) ;  ?>
+<?php header( 'Location: /Nike Zoom KD 12 Blue.html' ) ;  ?>
